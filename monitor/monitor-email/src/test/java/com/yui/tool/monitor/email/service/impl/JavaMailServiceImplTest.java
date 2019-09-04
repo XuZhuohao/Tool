e681b8ecc37b9c2b3eb5ee9e2ed106bdbf6e6d44
xuzhuohao <786725551@qq.com>
2019-09-04 13:59:16 +0800

    添加接收邮件测试代码

--- monitor/monitor-email/src/test/java/com/yui/tool/monitor/email/service/impl/JavaMailServiceImplTest.java
+++ monitor/monitor-email/src/test/java/com/yui/tool/monitor/email/service/impl/JavaMailServiceImplTest.java
@@ -2,10 +2,7 @@ package com.yui.tool.monitor.email.service.impl;
 
 
 import com.yui.tool.monitor.email.MonitorEmailApplicationTest;
-import com.yui.tool.monitor.email.dto.EmailEntity;
-import com.yui.tool.monitor.email.dto.FileEntity;
-import com.yui.tool.monitor.email.dto.ImageEntity;
-import com.yui.tool.monitor.email.dto.MailSenderDto;
+import com.yui.tool.monitor.email.dto.*;
 import com.yui.tool.monitor.email.service.JavaMailService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,25 +17,36 @@ public class JavaMailServiceImplTest extends MonitorEmailApplicationTest {
     @Test
     public void test() {
         MailSenderDto mailSenderDto = new MailSenderDto();
-        mailSenderDto.setEncoding("utf-8").setHost("xxx.xxx.xxx.xxx").setPort(25).setProtocol("smtp");
+        mailSenderDto.setEncoding("utf-8").setHost("xxx").setPort(25).setProtocol("smtp");
         final String key = JavaMailSenderService.addJavaMailSender(mailSenderDto);
-        EmailEntity emailEntity = new EmailEntity();
-        emailEntity.setContent("内容：email 测试<image src=\"cid:111\"/>").setFromUser("786725551@qq.com")
-                .setPassWord("passWord").setSubject("主题：测试")
-                .setToUser(new String[]{"test1@qq.com","test2@qq.com"});
+        SendEmailEntity sendEmailEntity = new SendEmailEntity();
+        sendEmailEntity.setContent("内容：email 测试<image src=\"cid:111\"/>").setFromUser("xxx@xxx.com.cn")
+                .setPassWord("password").setSubject("主题：测试")
+                .setToUser(new String[]{"xuxianxue@bluemoon.com.cn","huangshijie2@bluemoon.com.cn"});
 
         Set<FileEntity> files = new HashSet<>(16);
         files.add(new FileEntity().setFileName("附件1.jpg").setPath("D:\\data\\023 (4).jpg"));
-        emailEntity.setFiles(files);
+        sendEmailEntity.setFiles(files);
 
         Set<ImageEntity> images = new HashSet<>(16);
         images.add(new ImageEntity().setCid("111").setSrc("D:\\data\\023 (4).jpg"));
-        emailEntity.setImages(images);
+        sendEmailEntity.setImages(images);
 
-        emailEntity.setCopyTo(new String[]{"test3@qq.com","test4@qq.com"});
+        sendEmailEntity.setCopyTo(new String[]{"t1@xxx.com.cn","t2@xxx.com.cn"});
 
-        final boolean b = javaMailService.sendEmail(JavaMailSenderService.getJavaMailSender(key), emailEntity);
+        final boolean b = javaMailService.sendEmail(JavaMailSenderService.getJavaMailSender(key), sendEmailEntity);
         System.out.println("发送结果：" + b);
     }
 
+    @Test
+    public void testReceive(){
+        MailSenderDto mailSenderDto = new MailSenderDto();
+        // imps imp pop3 pop3s  s -> ssl
+        mailSenderDto.setEncoding("utf-8").setHost("xxx").setPort(101).setProtocol("imaps");
+        final String key = JavaMailSenderService.addJavaMailSender(mailSenderDto);
+        ReceiveEmailEntity entity = new ReceiveEmailEntity();
+        entity.setUsername("name@xxx.com.cn").setPassword("password");
+        javaMailService.receiveEmail(JavaMailSenderService.getJavaMailSender(key), entity);
+
+    }
 }
